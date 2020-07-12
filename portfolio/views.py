ce0a84c1e55900db22b24464b0f166d1e739ea81
olga_semenova <olgakonstantinova89@gmail.com>
2020-07-13 00:29:14 +0500

    Add project from folder Project

---
+++ portfolio/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+
+# Create your views here.
+def index(request):
+    tasks = Task.objects.order_by('-id')
+    return render(request, 'portfolio/index.html', {'title':'Главная страница сайта', 'tasks': tasks})
+
+
+def about(request):
+    return render(request, 'portfolio/about.html')
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была неверной'
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'portfolio/create.html', context)
